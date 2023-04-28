c91261bade83a962e8ed10ef784a74fb0955fd0f
Yuri Lopes <devyurilopes@gmail.com>
2023-04-28 14:14:31 +0100

    segundo envio

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let $campoItem = document.querySelector('.item__compra');
 let $campoQuantidade = document.querySelector('.quantidade__compra');
 let $btnEnviar = document.querySelector('#envia__registro');
+let $form = document.querySelector('.tabela__registros');
+let listaDeCompras = [];
 
 $btnEnviar.addEventListener('click', function (event) {
     event.preventDefault()
-    console.log($campoItem.value, $campoQuantidade.value);
+    $form.insertAdjacentHTML('beforeend', `
+    <tr>
+                <td> ${listaDeCompras.length} </td>
+                <td> ${$campoItem.value}  </td>
+                <td> ${$campoQuantidade.value}  </td>
+                <td> <button class="botaoDeleta"> X </button> </td>
+            </tr>
+    `)
 })
 
 
