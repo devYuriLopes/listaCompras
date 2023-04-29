07bd8446c63ca8286155723dda5bcff28375088f
Yuri Lopes <devyurilopes@gmail.com>
2023-04-29 14:01:43 +0100

    organização das pastas e criação de duas funções

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,6 @@
 let $campoItem = document.querySelector('.item__compra');
 let $campoQuantidade = document.querySelector('.quantidade__compra');
+let $campoValor = document.querySelector('.valor__compra');
 let $btnEnviar = document.querySelector('#envia__registro');
 let $form = document.querySelector('.tabela__registros');
 let listaDeCompras = [];
@@ -11,9 +12,26 @@ $btnEnviar.addEventListener('click', function (event) {
                 <td> ${listaDeCompras.length} </td>
                 <td> ${$campoItem.value}  </td>
                 <td> ${$campoQuantidade.value}  </td>
+                <td> ${$campoValor.value}  </td>
                 <td> <button class="botaoDeleta"> X </button> </td>
             </tr>
     `)
+    incrementaArray();
+    limpaCampos();
 })
 
+function limpaCampos() {
+    $campoItem.value = '';
+    $campoQuantidade.value = '';
+    $campoValor.value = '';
+}
 
+function incrementaArray(){
+    let item = {
+        item: $campoItem.value,
+        quantidade: $campoQuantidade.value,
+        valor: Number($campoValor.value)
+    }
+    listaDeCompras.push(item);
+    console.log(listaDeCompras)
+}
